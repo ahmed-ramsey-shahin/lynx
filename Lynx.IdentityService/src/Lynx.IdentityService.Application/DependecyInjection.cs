575d4797fdc0eb09c89cd189d5f098539220b03c
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-20 17:29:17 +0300

    Added DependecyInjection registration for the Application Layer.

---
+++ Lynx.IdentityService/src/Lynx.IdentityService.Application/DependecyInjection.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using FluentValidation;
+using Lynx.IdentityService.Application.Common.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lynx.IdentityService.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+                configuration.AddOpenBehavior(typeof(UnhandledExceptionBehavior<,>));
+                configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
+                configuration.AddOpenBehavior(typeof(PerformanceBehavior<,>));
+                configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                configuration.AddOpenBehavior(typeof(IdempotencyBehavior<,>));
+                configuration.AddOpenBehavior(typeof(CachingBehavior<,>));
+            });
+            return services;
+        }
+    }
+}
