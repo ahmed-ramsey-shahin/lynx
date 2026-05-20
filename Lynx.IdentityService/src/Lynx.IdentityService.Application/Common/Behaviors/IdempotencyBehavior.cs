21b562af5d51de475b7c5940fd0f318420e7b2f6
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-20 17:03:10 +0300

    Added IdempotencyBehavior (Application layer).

---
+++ Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Behaviors/IdempotencyBehavior.cs
@@ -0,0 +1,58 @@
+using Lynx.IdentityService.Application.Common.Interfaces;
+using Lynx.IdentityService.Application.Common.Services;
+using Lynx.IdentityService.Domain.Common.Results;
+using Lynx.IdentityService.Domain.Common.Results.Abstractions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Lynx.IdentityService.Application.Common.Behaviors
+{
+    public class IdempotencyBehavior<TRequest, TResponse>(
+        ICacheService cacheService,
+        ILogger<IdempotencyBehavior<TRequest, TResponse>> logger
+    ) : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : notnull
+        where TResponse : IResult
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is not IIdempotentCommand idempotentCommand)
+            {
+                return await next(cancellationToken);
+            }
+
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("Check cache for {RequestName} and {IdempotencyKey}.", typeof(TRequest).Name, idempotentCommand.IdempotencyKey);
+
+            var lockAcquired = await cacheService.TrySetAsync(idempotentCommand.IdempotencyKey, "InProgress", TimeSpan.FromMinutes(2), cancellationToken);
+
+            if (!lockAcquired)
+            {
+                if (logger.IsEnabled(LogLevel.Information))
+                    logger.LogInformation("{IdempotencyKey} already exists in the cache.", idempotentCommand.IdempotencyKey);
+
+                return (dynamic) Error.Conflict("Idempotency.Conflict", "Request is already being processed.");
+            }
+
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("{IdempotencyKey} was not found in the cache.", idempotentCommand.IdempotencyKey);
+
+            var result = await next(cancellationToken);
+
+            if (result.IsError)
+            {
+                if (logger.IsEnabled(LogLevel.Error))
+                    logger.LogError("An error happened during execution of the command. {@Errors}.", result.Errors);
+
+                await cacheService.RemoveAsync(idempotentCommand.IdempotencyKey, cancellationToken);
+                return result;
+            }
+
+            if (logger.IsEnabled(LogLevel.Information))
+                logger.LogInformation("Storing {IdempotencyKey} to cache.", idempotentCommand.IdempotencyKey);
+
+            await cacheService.SetAsync(idempotentCommand.IdempotencyKey, result, TimeSpan.FromMinutes(3), cancellationToken);
+            return result;
+        }
+    }
+}
--- Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Services/ICacheService.cs
+++ Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Services/ICacheService.cs
@@ -5,5 +5,6 @@ namespace Lynx.IdentityService.Application.Common.Services
         Task SetAsync<T>(string key, T value, TimeSpan? expiration=null, CancellationToken cancellationToken=default);
         Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken=default);
         Task RemoveAsync(string key, CancellationToken cancellationToken=default);
+        Task<bool> TrySetAsync<T>(string key, T value, TimeSpan? expiration=null, CancellationToken cancellationToken=default);
     }
 }
