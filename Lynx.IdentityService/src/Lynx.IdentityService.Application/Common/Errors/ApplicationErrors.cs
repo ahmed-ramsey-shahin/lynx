ed6343e168a06a2c2dc222ea34b4c247cffddfc1
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-20 19:01:43 +0300

    Implemented CreateUserCommandValidator.

--- Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Errors/ApplicationErrors.cs
+++ Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Errors/ApplicationErrors.cs
@@ -6,5 +6,8 @@ namespace Lynx.IdentityService.Application.Common.Errors
     {
         public static Error EmailAlreadyExists => Error.Conflict("User.EmailConflict", "A user with this email already exists.");
         public static Error UsernameAlreadyExists => Error.Conflict("User.UsernameConflict", "A user with this username already exists.");
+        public static Error EmailInvalid => Error.Validation("User.InvalidEmail", "The provided email address is invalid.");
+        public static Error UsernameInvalid => Error.Validation("User.InvalidUsername", "Invalid username. Use 3-16 characters, starting with a letter, and only letters, numbers, or underscores.");
+        public static Error PasswordInvalid => Error.Validation("User.InvalidPasswordFormat", "The password must be at least 8 characters long and include an uppercase letter, a lowercase letter, a number, and a special character.");
     }
 }
