6974778c3a1a1c32be2360e3362cf94462bbdbe6
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-20 19:20:28 +0300

    Added Update and GetUserById to the user repo/

--- Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Repositories/IUserRepository.cs
+++ Lynx.IdentityService/src/Lynx.IdentityService.Application/Common/Repositories/IUserRepository.cs
@@ -4,8 +4,10 @@ namespace Lynx.IdentityService.Application.Common.Repositories
 {
     public interface IUserRepository
     {
+        Task<User?> GetUserByIdAsync(Guid Id, CancellationToken cancellationToken=default);
         Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken=default);
         Task<bool> IsUsernameUniqueAsync(string email, CancellationToken cancellationToken=default);
         Task<bool> AddAsync(User user, CancellationToken cancellationToken=default);
+        Task<bool> UpdateAsync(User user, CancellationToken cancellationToken=default);
     }
 }
