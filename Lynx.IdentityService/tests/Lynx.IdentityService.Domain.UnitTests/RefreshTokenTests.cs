993b2a2b5aa7e03e0843e9f17ba2baa1a8144b13
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-21 23:03:17 +0300

    Added RefreshTokken.Revooke unit tests.

--- Lynx.IdentityService/tests/Lynx.IdentityService.Domain.UnitTests/RefreshTokenTests.cs
+++ Lynx.IdentityService/tests/Lynx.IdentityService.Domain.UnitTests/RefreshTokenTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using Lynx.IdentityService.Domain.Common.Results;
 using Lynx.IdentityService.Domain.Identity;
 
 namespace Lynx.IdentityService.Domain.UnitTests
@@ -49,7 +50,7 @@ namespace Lynx.IdentityService.Domain.UnitTests
         {
             // Arrange
             const string token = "RandomToken";
-            var expiresOn = DateTimeOffset.UtcNow.AddMinutes(1);
+            var expiresOn = DateTimeOffset.UtcNow.AddDays(-1);
 
             // Act
             var creationResult = RefreshToken.Create(token, expiresOn);
@@ -59,6 +60,52 @@ namespace Lynx.IdentityService.Domain.UnitTests
             creationResult.IsError.Should().BeTrue();
             creationResult.Errors.Should().ContainSingle()
                 .Which.Code.Should().Be(RefreshTokenErrors.ExpirationInvalid.Code);
+        }
+
+        [Fact]
+        public void Revoke_Should_ChangeRevokedAtAndIsRevoked_WhenNotAlreadyRevoked()
+        {
+            // Arrange
+            const string token = "RandomToken";
+            var expiresOn = DateTimeOffset.UtcNow.AddHours(2);
+            var creationResult = RefreshToken.Create(token, expiresOn);
+            var refreshToken = creationResult.Value;
+            var revokeTime = DateTimeOffset.UtcNow;
+
+            // Act
+            var revokeResult = refreshToken.Revoke(revokeTime);
+
+            // Assert
+            revokeResult.IsSuccess.Should().BeTrue();
+            revokeResult.IsError.Should().BeFalse();
+            revokeResult.Errors.Should().BeEmpty();
+            revokeResult.Value.Should().Be(Result.Updated);
+            refreshToken.IsRevoked.Should().BeTrue();
+            refreshToken.RevokedAt.Should().Be(revokeTime);
+        }
+
+        [Fact]
+        public void Revoke_Should_ReturnUpdatedAndNothingChanges_WhenAlreadyRevoked()
+        {
+            // Arrange
+            const string token = "RandomToken";
+            var expiresOn = DateTimeOffset.UtcNow.AddHours(2);
+            var creationResult = RefreshToken.Create(token, expiresOn);
+            var refreshToken = creationResult.Value;
+            var revokeTime = DateTimeOffset.UtcNow;
+            var reRevokeTime = DateTimeOffset.UtcNow.AddHours(3);
+            refreshToken.Revoke(revokeTime);
+
+            // Act
+            var revokeResult = refreshToken.Revoke(reRevokeTime);
+
+            // Assert
+            revokeResult.IsSuccess.Should().BeTrue();
+            revokeResult.IsError.Should().BeFalse();
+            revokeResult.Errors.Should().BeEmpty();
+            revokeResult.Value.Should().Be(Result.Updated);
+            refreshToken.IsRevoked.Should().BeTrue();
+            refreshToken.RevokedAt.Should().Be(revokeTime);
         }
     }
 }
