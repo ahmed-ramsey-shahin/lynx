8a263cae8599c788df6b2d9f479730e14c70c1de
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-21 22:37:15 +0300

    Added domain and application layers unit testing (Not Implemented Yet).

---
+++ Lynx.IdentityService/tests/Lynx.IdentityService.Domain.UnitTests/RefreshTokenTests.cs
@@ -0,0 +1,47 @@
+using FluentAssertions;
+using Lynx.IdentityService.Domain.Identity;
+
+namespace Lynx.IdentityService.Domain.UnitTests
+{
+    public class RefreshTokenTests
+    {
+        [Fact]
+        public void Create_Should_ReturnRefreshToken_WhenParametersAreValid()
+        {
+            // Arrange
+            const string tokenString = "some_random_token";
+            var expiresOn = DateTimeOffset.UtcNow.AddDays(7);
+
+            // Act
+            var creationResult = RefreshToken.Create(tokenString, expiresOn);
+
+            // Assert
+            creationResult.IsSuccess.Should().BeTrue();
+            var refreshToken = creationResult.Value;
+            refreshToken.Token.Should().Be(tokenString);
+            refreshToken.ExpiresOn.Should().Be(expiresOn);
+            refreshToken.IsRevoked.Should().BeFalse();
+            refreshToken.RevokedAt.Should().BeNull();
+            refreshToken.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void Create_Should_ReturnTokenRequiredError_WhenTokenIsNullOrWhitespace(string token)
+        {
+            // Arrange
+            var expiresOn = DateTimeOffset.UtcNow.AddDays(7);
+
+            // Act
+            var creationResult = RefreshToken.Create(token, expiresOn);
+
+            // Assert
+            creationResult.IsSuccess.Should().BeFalse();
+            creationResult.IsError.Should().BeTrue();
+            creationResult.Errors.Should().ContainSingle()
+                .Which.Code.Should().Be(RefreshTokenErrors.TokenRequired.Code);
+        }
+    }
+}
