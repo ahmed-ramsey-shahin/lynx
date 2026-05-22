cc25f08cd86d0507e1c5d13ab3f977f0f43300ae
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-23 00:14:26 +0300

    Added MessagePublishingServiceMockBuilder mock.

---
+++ Lynx.IdentityService/tests/Lynx.IdentityService.Application.UnitTests/MockBuilders/MessagePublishingServiceMockBuilder.cs
@@ -0,0 +1,20 @@
+using Lynx.IdentityService.Application.Common.Services;
+using Moq;
+
+namespace Lynx.IdentityService.Application.UnitTests.MockBuilders
+{
+    public class MessagePublishingServiceMockBuilder
+    {
+        public Mock<IMessagePublishingService> Mock { get; } = new(MockBehavior.Strict);
+        public IMessagePublishingService Object => Mock.Object;
+
+        public MessagePublishingServiceMockBuilder WithSuccessfulPublish<T>(string? queue=null, T? body=default)
+        {
+            Mock.Setup(service => service.PublishAsync(
+                It.Is<string>(v => queue == null || queue == v),
+                It.Is<T>(v => body == null || body.Equals(v))
+            ));
+            return this;
+        }
+    }
+}
