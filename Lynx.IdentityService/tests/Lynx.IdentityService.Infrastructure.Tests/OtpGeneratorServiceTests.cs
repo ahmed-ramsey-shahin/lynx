07887d32abe6fe9b0fca0d64372626a40cb69a3d
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-27 01:23:34 +0300

    Added unit tests for OtpGeneratorService.

---
+++ Lynx.IdentityService/tests/Lynx.IdentityService.Infrastructure.Tests/OtpGeneratorServiceTests.cs
@@ -0,0 +1,60 @@
+using FluentAssertions;
+using Lynx.IdentityService.Infrastructure.Services;
+
+namespace Lynx.IdentityService.Infrastructure.Tests
+{
+    public class OtpGeneratorServiceTests
+    {
+        private readonly OtpGeneratorService? _generatorService = new();
+
+        [Fact]
+        public void GenerateResetCode_Should_ReturnUniqueCodes()
+        {
+            // Arrange
+            HashSet<string> set = [];
+
+            // Act
+            for (int i = 0; i < 100; i++)
+            {
+                set.Add(_generatorService!.GenerateResetCode());
+            }
+
+            // Assert
+            set.Should().HaveCount(100);
+        }
+
+        [Fact]
+        public void GenerateResetCode_Should_Return6DigitsString()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _generatorService!.GenerateResetCode().Should().MatchRegex("^[0-9]{6}$");
+            }
+        }
+
+        [Fact]
+        public void GenerateUrlSafeToken_Should_ReturnUniqueCodes()
+        {
+            // Arrange
+            HashSet<string> set = [];
+
+            // Act
+            for (int i = 0; i < 100; i++)
+            {
+                set.Add(_generatorService!.GenerateUrlSafeToken());
+            }
+
+            // Assert
+            set.Should().HaveCount(100);
+        }
+
+        [Fact]
+        public void GenerateUrlSafeToken_Should_Return32DigitHexNumber()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _generatorService!.GenerateUrlSafeToken().Should().MatchRegex("^[0-9a-fA-F]{32}$");
+            }
+        }
+    }
+}
