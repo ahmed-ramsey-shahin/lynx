e638513fe370473f2a1adbc7efd74a411845fba9
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-27 01:49:25 +0300

    Added more testcases to PasswordHashingServiceTests.

--- Lynx.IdentityService/tests/Lynx.IdentityService.Infrastructure.Tests/PasswordHashingServiceTests.cs
+++ Lynx.IdentityService/tests/Lynx.IdentityService.Infrastructure.Tests/PasswordHashingServiceTests.cs
@@ -7,6 +7,18 @@ namespace Lynx.IdentityService.Infrastructure.Tests
     {
         private readonly PasswordHashingService _hashingService = new("random_pepper");
 
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Hash_Should_ThrowArgumentException_WhenPasswordIsEmpty(string password)
+        {
+            // Arrange
+            Action action = () => _hashingService.Hash(password);
+
+            // Act & Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
         [Fact]
         public void Hash_Should_ReturnUniqueHasheForThePassword()
         {
@@ -34,8 +46,18 @@ namespace Lynx.IdentityService.Infrastructure.Tests
             hash.Should().NotBe(password);
         }
 
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("", " ")]
+        [InlineData(" ", "")]
+        [InlineData(" ", " ")]
+        public void Verify_Should_ReturnFalse_WhenEitherPasswordOrHashIsEmpty(string password, string hash)
+        {
+            _hashingService.Verify(password, hash).Should().BeFalse();
+        }
+
         [Fact]
-        public void Validate_Should_ReturnTrue_WhenValidatingTheCorrectPassword()
+        public void Verify_Should_ReturnTrue_WhenValidatingTheCorrectPassword()
         {
             // Arrange
             const string password = "password";
@@ -48,7 +70,7 @@ namespace Lynx.IdentityService.Infrastructure.Tests
         }
 
         [Fact]
-        public void Validate_Should_ReturnFalseIf_WheValidatingTheWrongPassword()
+        public void Verify_Should_ReturnFalse_WheValidatingTheWrongPassword()
         {
             // Arrange
             const string password = "password!";
