bf245d24e7228035a575cd8af61d993debddb95e
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-26 21:53:06 +0300

    Added MessagePublishingService.

--- Lynx.IdentityService/src/Lynx.IdentityService.Infrastructure/Services/RabbitMq/MessagePublishingService.cs
+++ Lynx.IdentityService/src/Lynx.IdentityService.Infrastructure/Services/RabbitMq/MessagePublishingService.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using Lynx.IdentityService.Application.Common.Services;
+using RabbitMQ.Client;
 
 namespace Lynx.IdentityService.Infrastructure.Services.RabbitMq
 {
@@ -6,9 +8,43 @@ namespace Lynx.IdentityService.Infrastructure.Services.RabbitMq
         IRabbitMqChannelPool channelPool
     ) : IMessagePublishingService
     {
-        public async Task PublishAsync<TValue>(string queue, TValue body)
+        private async Task PublishAsync(string queue, byte[] bytes, string contentType, CancellationToken cancellationToken)
         {
-            //
+            var channel = await channelPool.GetChannelAsync(cancellationToken);
+            var props = new BasicProperties()
+            {
+                DeliveryMode = DeliveryModes.Persistent,
+                ContentType = contentType,
+                AppId = "IdentityService"
+            };
+
+            try
+            {
+                await channel.BasicPublishAsync(
+                    string.Empty,
+                    queue,
+                    false,
+                    props,
+                    bytes,
+                    cancellationToken
+                );
+            }
+            finally
+            {
+                channelPool.ReturnChannel(channel);
+            }
+        }
+
+        public async Task PublishAsync(string queue, Guid body, CancellationToken cancellationToken=default)
+        {
+            var message = body.ToByteArray();
+            await PublishAsync(queue, message, "application/octet-stream", cancellationToken);
+        }
+
+        public async Task PublishAsync<TValue>(string queue, TValue body, CancellationToken cancellationToken=default)
+        {
+            var message = JsonSerializer.SerializeToUtf8Bytes(body);
+            await PublishAsync(queue, message, "application/json", cancellationToken);
         }
     }
 }
