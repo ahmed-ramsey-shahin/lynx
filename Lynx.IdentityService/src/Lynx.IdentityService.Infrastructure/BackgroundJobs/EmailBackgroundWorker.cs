c63acc8540dd22f52b4760f9e86768aa7cf1a8ed
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-05-28 02:47:11 +0300

    EmailBackgroundWorker was injecting IEmailService even tho EmailBackgroundWorker is Singleton.

--- Lynx.IdentityService/src/Lynx.IdentityService.Infrastructure/BackgroundJobs/EmailBackgroundWorker.cs
+++ Lynx.IdentityService/src/Lynx.IdentityService.Infrastructure/BackgroundJobs/EmailBackgroundWorker.cs
@@ -1,5 +1,6 @@
 using Lynx.IdentityService.Application.Common.BackgroundJobs;
 using Lynx.IdentityService.Application.Common.Services;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -7,7 +8,7 @@ namespace Lynx.IdentityService.Infrastructure.BackgroundJobs
 {
     public class EmailBackgroundWorker(
         IEmailBackgroundQueue emailQueue,
-        IEmailService emailService,
+        IServiceScopeFactory scopeFactory,
         ILogger<EmailBackgroundWorker> logger
     ) : BackgroundService
     {
@@ -21,6 +22,8 @@ namespace Lynx.IdentityService.Infrastructure.BackgroundJobs
                 {
                     try
                     {
+                        using var scope = scopeFactory.CreateScope();
+                        var emailService = scope.ServiceProvider.GetRequiredService<IEmailService>();
                         if (logger.IsEnabled(LogLevel.Information))
                             logger.LogInformation("Sending background email {@EmailJob}.", job);
                         await emailService.SendEmailAsync(
