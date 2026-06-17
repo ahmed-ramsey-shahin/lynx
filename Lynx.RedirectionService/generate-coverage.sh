b1e7f61871888fa76a640ae6b032e959da10f864
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-06-17 15:30:00 +0300

    Added empty clean architecture project structure for the redirection service.

---
+++ Lynx.RedirectionService/generate-coverage.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+dotnet test --collect:"XPlat Code Coverage"
+reportgenerator -reports:"**/coverage.cobertura.xml" -targetdir:"coverage-report" -reporttypes:Html
+open coverage-report/index.html
