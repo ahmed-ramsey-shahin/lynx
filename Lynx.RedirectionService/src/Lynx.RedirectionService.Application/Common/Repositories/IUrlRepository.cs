c43f078b0b848907536fe666fdf17854bff70a51
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-06-18 13:32:33 +0300

    Added IUrlRepository.

---
+++ Lynx.RedirectionService/src/Lynx.RedirectionService.Application/Common/Repositories/IUrlRepository.cs
@@ -0,0 +1,12 @@
+using Lynx.RedirectionService.Domain.Urls;
+
+namespace Lynx.RedirectionService.Application.Common.Repositories
+{
+    public interface IUrlRepository
+    {
+        Task<Url?> GetUrlByAlias(string alias, CancellationToken cancellationToken=default);
+        Task<Url?> GetUrlById(Guid id, CancellationToken cancellationToken=default);
+        Task<bool> AddAsync(Url url, CancellationToken cancellationToken=default);
+        Task<bool> DeleteUrlByAlias(string alias, CancellationToken cancellationToken=default);
+    }
+}
