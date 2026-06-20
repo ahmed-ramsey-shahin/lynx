48edb6c750df781eaf1ef0e7382bc8ac6a82bba8
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-06-20 10:44:43 +0300

    Added program.cs configuration.

--- Lynx.RedirectionService/src/Lynx.RedirectionService.Api/Program.cs
+++ Lynx.RedirectionService/src/Lynx.RedirectionService.Api/Program.cs
@@ -1,7 +1,30 @@
+using Lynx.RedirectionService.Infrastructure;
+using Hangfire;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddInfrastructureLayer(config)
+    .AddHttpContextAccessor()
+    .AddControllers();
+builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseHangfireDashboard("/hangfire", new DashboardOptions
+    {
+        Authorization = [],
+    });
+}
+else
+{
+    app.UseHsts();
+}
+
+await app.Services.ConfigureMongoDbAsync();
+
+app.MapControllers();
 
 app.Run();
