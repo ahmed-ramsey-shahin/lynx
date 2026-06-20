03689f777d7ac5292cbb38917199fa5530035c70
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-06-20 10:44:31 +0300

    Added Mongo to DI.

--- Lynx.RedirectionService/src/Lynx.RedirectionService.Infrastructure/DependencyInjection.cs
+++ Lynx.RedirectionService/src/Lynx.RedirectionService.Infrastructure/DependencyInjection.cs
@@ -4,11 +4,28 @@ using Lynx.RedirectionService.Infrastructure.BackgroundServices;
 using Lynx.RedirectionService.Infrastructure.Services.RabbitMq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Lynx.RedirectionService.Infrastructure.Data.Configuration;
+using MongoDB.Driver;
+using Lynx.RedirectionService.Infrastructure.Data;
+using Lynx.RedirectionService.Application.Common.Repositories;
 
 namespace Lynx.RedirectionService.Infrastructure
 {
     public static class DependencyInjection
     {
+        public static async Task ConfigureMongoDbAsync(this IServiceProvider services)
+        {
+            MongoDbConfiguration.ConfigureMappings();
+            var client = services.GetRequiredService<IMongoClient>();
+            await MongoDbIndexConfiguration.ConfigureUniqueIndexesAsync(client);
+        }
+
+        private static IServiceCollection AddMongoDb(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+            services.AddScoped<IUrlRepository, UrlRepository>();
+            return services;
+        }
         private static IServiceCollection AddRabbitMQ(this IServiceCollection services, string connectionString)
         {
             services.AddSingleton<IRabbitMqConnectionManager>(_ => new RabbitMqConnectionManager(connectionString));
@@ -22,8 +39,10 @@ namespace Lynx.RedirectionService.Infrastructure
         }
         public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration config)
         {
+            var mongoDbConnectionString = config.GetConnectionString("MongoDB") ?? throw new InfrastructureConfigurationException("ConnectionStrings:MongoDB");
             var rabbitMqConnectionString = config.GetConnectionString("RabbitMQ") ?? throw new InfrastructureConfigurationException("ConnectionStrings:RabbitMQ");
-            services.AddRabbitMQ(rabbitMqConnectionString);
+            services.AddRabbitMQ(rabbitMqConnectionString)
+                .AddMongoDb(mongoDbConnectionString);
             return services;
         }
     }
