6fb1abbec2d7a9e97f7910870afd81fe18e6c1f7
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2026-06-24 21:19:03 +0300

    Added MessagePublishingServiceTests.

---
+++ Lynx.RedirectionService/tests/Lynx.RedirectionService.Infrastructure.Tests/MessagePublishingServiceTests.cs
@@ -0,0 +1,114 @@
+using System.Text.Json.Nodes;
+using FluentAssertions;
+using Lynx.RedirectionService.Infrastructure.Services.RabbitMq;
+using Lynx.RedirectionService.Infrastructure.Tests.Fixtures;
+
+namespace Lynx.RedirectionService.Infrastructure.Tests
+{
+    [Collection("RabbitMqCollection")]
+    public class MessagePublishingServiceTests(RabbitMqFixture fixture) : IAsyncLifetime
+    {
+        private readonly IRabbitMqChannelPool _channelPool = fixture.ChannelPool;
+        private MessagePublishingService? _messageService;
+        private readonly List<string> _testQueues = ["DeletedUsers"];
+
+        public async Task InitializeAsync()
+        {
+            _messageService = new(_channelPool);
+            var channel = await _channelPool.GetChannelAsync();
+
+            try
+            {
+                foreach (var queue in _testQueues)
+                {
+                    await channel.QueueDeclareAsync(
+                        queue: queue,
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false
+                    );
+                }
+            }
+            finally
+            {
+                await _channelPool.ReturnChannel(channel);
+            }
+        }
+
+
+        [Fact]
+        public async Task PublishAsync_Should_AddMessageToTheRabbitMq_WhenBodyIsGuid()
+        {
+            // Arrange
+            var body = Guid.NewGuid();
+            var queueName = _testQueues[0];
+
+            // Act
+            await _messageService!.PublishAsync(queueName, body, default);
+
+            // Assert
+            var channel = await _channelPool.GetChannelAsync();
+
+            try
+            {
+                var result = await channel.BasicGetAsync(queueName, true);
+                result.Should().NotBeNull();
+                result.Body.Should().BeEquivalentTo(body.ToByteArray());
+            }
+            finally
+            {
+                await _channelPool.ReturnChannel(channel);
+            }
+        }
+
+        [Fact]
+        public async Task PublishAsync_Should_AddMessageToTheRabbitMq_WhenBodyIsObject()
+        {
+            // Arrange
+            var body = new
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestUser"
+            };
+            var queueName = _testQueues[0];
+
+            // Act
+            await _messageService!.PublishAsync(queueName, body, default);
+
+            // Assert
+            var channel = await _channelPool.GetChannelAsync();
+
+            try
+            {
+                var result = await channel.BasicGetAsync(queueName, true);
+                result.Should().NotBeNull();
+                var jsonNode = JsonNode.Parse(result.Body.ToArray());
+                var receivedId = jsonNode!["Id"]!.GetValue<Guid>();
+                var receivedName = jsonNode!["Name"]!.GetValue<string>();
+                receivedId.Should().Be(body.Id);
+                receivedName.Should().Be(body.Name);
+            }
+            finally
+            {
+                await _channelPool.ReturnChannel(channel);
+            }
+        }
+
+        public async Task DisposeAsync()
+        {
+            var channel = await _channelPool.GetChannelAsync();
+
+            try
+            {
+                foreach (var queue in _testQueues)
+                {
+                    await channel.QueuePurgeAsync(queue);
+                }
+            }
+            finally
+            {
+                await _channelPool.ReturnChannel(channel);
+            }
+        }
+    }
+}
